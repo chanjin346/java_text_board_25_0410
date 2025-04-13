ea1fb90bcae2bab34a28874fdb38642fcbd6276f
chanjin346 <ckswls2231@gmail.com>
2025-04-13 16:28:16 +0900

    게시물 작성 시 제목, 내용에 대한 입력 유무 처리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -9,37 +9,41 @@ public class Main {
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("== 텍스트 게시판 시작합니다. ==");
 
-    while (true){
+    while (true) {
       System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("/usr/article/write")){
+      if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
         System.out.print("제목 : ");
         String subject = sc.nextLine();
 
+        if (subject.trim().isEmpty()) {
+          System.out.println("제목을 입력해주세요.");
+          continue;
+        }
+
         System.out.print("내용 : ");
         String content = sc.nextLine();
 
+        if (content.trim().isEmpty()) {
+          System.out.println("내용을 입력해주세요.");
+          continue;
+        }
+
         int id = 1;
 
-        System.out.printf("%d번 게시물이 등록되었습니다.", id);
-      }
-      else if(cmd.equals("exit")){
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      } else if (cmd.equals("exit")) {
         System.out.println("== 텍스트 게시판 종료합니다. ==");
         break;
-      }
-      else {
+      } else {
         System.out.println("잘못 입력된 명령어입니다.");
       }
     }
 
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
 
-
-
-
-
-
-
+    sc.close();
   }
 }
