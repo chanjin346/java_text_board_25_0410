6782301378b2a5c913fd2722a35a2aa2a0128245
chanjin346 <ckswls2231@gmail.com>
2025-04-13 15:07:47 +0900

    시작 메시지 출력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -2,6 +2,10 @@ package com.sbs.java.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("== 텍스트 게시판 시작합니다. ==");
+    System.out.println("== 텍스트 게시판 종료합니다. ==");
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
+
   }
 }
